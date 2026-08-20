9acf109937377d7a5409790e63d2e435bdf71179
chitranjaniimet-tech <chitranjan.iimet@gmail.com>
2026-08-20 23:39:02 +0530

    Restore R8 resolution and persist CI diagnostics

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // Official standalone R8 Maven repository. PocketJava embeds D8/R8 APIs
+        // because Java bytecode must be converted to DEX on-device at runtime.
+        maven("https://storage.googleapis.com/r8-releases/raw")
     }
 }
 
